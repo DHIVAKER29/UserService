e9b99c2b38e3b163840df8cefb03188ad498d3e3
DHIVAKER29 <ssdddhivaker@gmail.com>
2024-09-26 21:44:30 +0530

    Added DTOs and Repository

--- src/main/java/com/scaler/userservice/service/UserService.java
+++ src/main/java/com/scaler/userservice/service/UserService.java
@@ -1,14 +1,30 @@
 package com.scaler.userservice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scaler.userservice.configs.KafkaProducerClient;
+import com.scaler.userservice.repository.TokenRepository;
 import com.scaler.userservice.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    UserRepository userRepository;
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private TokenRepository tokenRepository;
+    private KafkaProducerClient kafkaProducerClient;
+    private ObjectMapper objectMapper;
 
 
-    UserService(UserRepository userRepository) {
+    UserService(UserRepository userRepository,
+                BCryptPasswordEncoder bCryptPasswordEncoder,
+                TokenRepository tokenRepository,
+                KafkaProducerClient kafkaProducerClient,
+                ObjectMapper objectMapper) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.tokenRepository = tokenRepository;
+        this.kafkaProducerClient = kafkaProducerClient;
+        this.objectMapper = objectMapper;
     }
 }
